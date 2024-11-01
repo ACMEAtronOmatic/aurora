849daaef40f81b9830074e3b81b24425f3710c2c
James Gibbons <james@myradar.com>
2024-11-01 10:57:24 -0400

    [SCI-701] Aurora gif output, extended rollout

--- activate_env.sh
+++ activate_env.sh
@@ -1,3 +1,3 @@
 source a_env/bin/activate
-pip install cdsapi xarray netcdf4 matplotlib pathlib cartopy
+pip install cdsapi xarray netcdf4 matplotlib pathlib cartopy imageio
 pip install -e ".[dev]"
