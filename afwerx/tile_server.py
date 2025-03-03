fe80d10a2a138868d991e5daeca3dc7205e47f8b
James Gibbons <james@myradar.com>
2025-03-03 11:11:19 -0500

    [SCI-807] working xesmf import

--- afwerx/tile_server.py
+++ afwerx/tile_server.py
@@ -10,13 +10,11 @@ import warnings
 import matplotlib.pyplot as plt
 from cartopy import crs as ccrs, feature as cfeature
 from visualize import plot_xr, rain_palette
+import yaml
 
 
 # NOTE: must be installed through conda
-try:
-    import xesmf as xe
-except:
-    warnings.warn("xesmf not installed")
+import xesmf as xe
 
 
 NUM_CORES = multiprocessing.cpu_count()
@@ -39,22 +37,76 @@ maxY = maxTileY * TILE_SIZE
 SKIP_PLOT = ["latitude", "longitude", "time", "batch", "history", "rollout_step", "level"]
 
 
-def regrid(data : xr.Dataset, regrid_file : str) -> xr.Dataset:
+def regrid(data : xr.Dataset, regrid_file : str, conus : bool = False) -> xr.Dataset:
     '''
     Interpolate 0.25 degree GFS/Aurora/Mercator data to GOES CONUS grid
 
     Parameters
     ----------
-    data : xr.DataSet
+    data : xr.Dataset
         Xarray dataset to be regridded
     regrid_file : str
         Full path to the xESMF regridding file. 
         Likely GFS/Aurora/Mercator 0.25 degree to GOES CONUS Zoom 7
+    conus : bool = False
+        Whether to regrid to the CONUS extent
     '''
 
+    # Create a dummy target dataset of the CONUS extent
+    # MRMS CONUS domain boundaries
+    if conus:
+        lat_min, lat_max = 20.0, 55.0  # degrees North
+        lon_min, lon_max = -130.0, -60.0  # degrees West (converted to negative values)
+    else:
+        # global extent
+        lat_min, lat_max = -90.0, 90.0  # degrees North
+        lon_min, lon_max = -180.0, 180.0  # degrees West
+
+    # Create coordinates at MRMS resolution (0.01°)
+    lat = np.arange(lat_min, lat_max + 0.01, 0.01)
+    lon = np.arange(lon_min, lon_max + 0.01, 0.01)
+
+    # Create the xarray Dataset with proper attributes
+    ds_dummy = xr.Dataset(
+        coords={
+            "lat": (["lat"], lat, {"units": "degrees_north"}),
+            "lon": (["lon"], lon, {"units": "degrees_east"}),
+        }
+    )
+
+    # Print the shapes of the data and the dummy dataset
+    print("Data Lat Range: ", type(data['latitude'].min().values), data['latitude'].min().values, " - to - ", data['latitude'].max().values)
+    print("Data Lon Range: ", type(data['longitude'].min().values), data['longitude'].min().values, " - to - ", data['longitude'].max().values)
+
+    # Get shape of latitude and longitude
+    print("Data Shape: ", len(data['latitude']), len(data['longitude']))
+
+    print("Dummy Lat Range: ", type(ds_dummy['lat'].min().values), ds_dummy['lat'].min().values, " - to - ", ds_dummy['lat'].max().values)
+    print("Dummy Lon Range: ", type(ds_dummy['lon'].min().values), ds_dummy['lon'].min().values, " - to - ", ds_dummy['lon'].max().values)
+    print("Dummy Shape: ", len(ds_dummy['lat']), len(ds_dummy['lon']))
 
 
-    pass
+    # Open and inspect the weights file
+    weights_ds = xr.open_dataset(regrid_file)
+
+    print("Weights File: ")
+    print(weights_ds)
+
+
+    regridder = xe.Regridder(data, ds_dummy, method = 'bilinear', weights = regrid_file, reuse_weights=True)
+
+    # Regrid
+    ds_regridded = regridder(data)
+
+    regridder.grid_in.destroy()
+    regridder.grid_out.destroy()
+
+    # Print new shape, latitude, and longitude range
+    print("New Lat Range: ", type(ds_regridded['latitude'].min().values), ds_regridded['latitude'].min().values, " - to - ", ds_regridded['latitude'].max().values)
+    print("New Lon Range: ", type(ds_regridded['longitude'].min().values), ds_regridded['longitude'].min().values, " - to - ", ds_regridded['longitude'].max().values)
+    print("Shape: ", ds_regridded.shape)
+
+    return ds_regridded
 
 
 
@@ -153,15 +205,13 @@ def tile_into_folders(serve_dir, noun, timestamp, image):
                                     x*TILE_SIZE-minX : x*TILE_SIZE-minX+TILE_SIZE])
                 
 
-if __name__ == '__main__':
-    serve_dir = "serve"
-    noun = "test"
-    timestamp = '20240101T0000Z'
+def process_aurora_preds(filename : str):
+    ds = xr.open_dataset(filename)
 
-    ds = xr.open_dataset('tiles/test_preds.h5')
     ds = ds.assign_coords(longitude=(((ds.longitude + 180) % 360) - 180))
-
-
+    ds = ds.assign_attrs({'latitude': {'units': 'degrees_north'},
+                        'longitude': {'units': 'degrees_east'}})
+    
     # Print ranges for lat, lon, time, and level
     print("Lat Range: ", type(ds['latitude'].min().values), ds['latitude'].min().values, " - to - ", ds['latitude'].max().values)
     print("Lon Range: ", type(ds['longitude'].min().values), ds['longitude'].min().values, " - to - ", ds['longitude'].max().values)
@@ -172,15 +222,10 @@ if __name__ == '__main__':
     print("Variables: ")
     for var in ds.variables:
         print("\t", var, ds[var].shape)
+    
+    return ds
 
-
-    # Print lat lon arrays
-    # lats = ds['latitude'].values
-    # lons = ds['longitude'].values
-
-    # print("Lats: ", lats)
-    # print("Lons: ", lons)
-
+def select_conus(ds):
     # Extract only CONUS data from the xarray dataset
     minLat = 55.0
     maxLat = 20.0
@@ -192,23 +237,38 @@ if __name__ == '__main__':
     print("New Lat Range: ", type(ds['latitude'].min().values), ds['latitude'].min().values, " - to - ", ds['latitude'].max().values)
     print("New Lon Range: ", type(ds['longitude'].min().values), ds['longitude'].min().values, " - to - ", ds['longitude'].max().values)
 
-    # Plot plot some variables as a sanity check
-    for v in ['atmos_v', 'surf_2t', 'static_z']:
-        if v in SKIP_PLOT:
-            continue
+    return ds
 
-        print("Plotting variable: ", v)
-        # Check if the variable is atmospheric, has levels
-        if 'level' in ds[v].dims:
-            for l in [1000, 700, 300]:
-                plot_xr(ds, var=v, level=l)
-                plot_xr(ds, var=v, level=l, with_crs=False)
-        else:
-            plot_xr(ds, var=v)
-            plot_xr(ds, var=v, with_crs=False)
+if __name__ == '__main__':
+    serve_dir = "serve"
+    noun = "test"
+    timestamp = '20240101T0000Z'
+    visualize = False
 
+    with open("configs/configs.yml", 'r') as file:
+        config = yaml.safe_load(file)
 
+    ds = process_aurora_preds('tiles/test_preds.h5')
 
+    ds = select_conus(ds)
+
+    if visualize:
+        # Plot plot some variables as a sanity check
+        for v in ['atmos_v', 'surf_2t', 'static_z']:
+            if v in SKIP_PLOT:
+                continue
+
+            print("Plotting variable: ", v)
+            # Check if the variable is atmospheric, has levels
+            if 'level' in ds[v].dims:
+                for l in [1000, 700, 300]:
+                    plot_xr(ds, var=v, level=l)
+                    plot_xr(ds, var=v, level=l, with_crs=False)
+            else:
+                plot_xr(ds, var=v)
+                plot_xr(ds, var=v, with_crs=False)
+
+    ds_regridded = regrid(data=ds, regrid_file=config['inference']['interpolation_file'], conus=True)
 
     # tile_into_folders(serve_dir, noun, timestamp, image)
 
